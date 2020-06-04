0889ca588e60976d4319bb34f800b2f271939ee8
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2020-06-04 06:14:46 +0530

    Update Inventory issues

--- build/10.bundle.js
+++ build/10.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{401:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Home")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{543:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Home")}}}]);
