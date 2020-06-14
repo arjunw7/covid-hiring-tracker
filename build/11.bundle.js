bf09a71c04f9c88ef995a6ee11e83c5d694b09fd
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2020-06-14 20:05:14 +0530

    Update inv sccreen

--- build/11.bundle.js
+++ build/11.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{559:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Home")}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{558:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"Home")}}}]);
