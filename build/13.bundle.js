687373638e6f2a5cdddf4c46df200a0d54dec291
Arjun Wadhwa <arjun.wadhwa@walmartlabs.com>
2020-06-23 23:59:32 +0530

    minor fixes

--- build/13.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{566:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"NotFound")}}}]);
---
+++ build/precache-manifest.7fe450bf8cdfb843a0a706083b33074c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8857b86ab37b85175509704325278bf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d7b310a0c3d30036e5e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d73cff072796236f3641",
+    "url": "/static/css/main.7cfbd7f3.chunk.css"
+  },
+  {
+    "revision": "4d7b310a0c3d30036e5e",
+    "url": "/static/js/2.1d547527.chunk.js"
+  },
+  {
+    "revision": "d73cff072796236f3641",
+    "url": "/static/js/main.b5e5c621.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e290fefd987d07af79c6a736d4119eca",
+    "url": "/static/media/walmart-logo-home.e290fefd.png"
+  }
+]);
